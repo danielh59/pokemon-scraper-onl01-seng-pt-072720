3a1818c2c95584ae96598612bd65ca8ebd0a0b1b
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-09 01:35:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,8 +10,14 @@ class Pokemon
      @db = db
    end
 
-   def self.save(name, type, the_db)
+  def self.save(name, type, the_db)
   the_db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
+  def self.find(id, the_db)
+  pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+  name = pokemon[1]
+  type = pokemon[2]
+  hp = pokemon[3]
+  
 end
